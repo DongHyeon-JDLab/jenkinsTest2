18514ff79cb4f685c6adb51f50fdf0b16ef7735f
DH Kim <dhkim@myhost.local>
2015-07-26 20:47:16 +0900

    create Xcode project

---
+++ jenkinsTest/jenkinsTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  jenkinsTest
+//
+//  Created by DH Kim on 2015. 7. 26..
+//  Copyright (c) 2015년 DH Kim. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ jenkinsTest/jenkinsTest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  jenkinsTest
+//
+//  Created by DH Kim on 2015. 7. 26..
+//  Copyright (c) 2015년 DH Kim. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
